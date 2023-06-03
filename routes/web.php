fe01e8e3581cdcda0fc3f33c11a82832251eadb5
nada rafat <nadarmohamed.97@gmail.com>
2023-06-04 00:25:04 +0300

    create invoice model&migration file&controller&index view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-
-Auth::routes(['register'=>false]);
-Route::get('/{page}',[AdminController::class,'index']);
+Auth::routes(['register' => false]);
+Route::resource('invoices',InvoiceController::class);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/{page}', [AdminController::class, 'index']);
